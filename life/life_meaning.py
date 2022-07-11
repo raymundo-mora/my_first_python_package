1576938269f0fa407ac147f5840714e91c013ded
ray <mora.r98@gmail.com>
2022-07-11 10:43:31 -0400

    completed exercise

---
+++ life/life_meaning.py
@@ -0,0 +1,9 @@
+"""
+Authors: Raymundo Mora, Brandtly Jones
+Class: DS5100
+"""
+def life_meaning():
+    """This complex algorithm computes the meaning of life
+    and returns the"""
+    print(42)
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="my_first_python_package",
+      version=0.1,
+      description="Answers to life's deepest question",
+      url="https://github.com/raymundo-mora/my_first_python_package",
+      author=['Raymundo Mora','Brandtly Jones'],
+      author_email=['rm3xw@virginia.edu','bnj8f@virginia.edu'],
+      license='MIT',
+      packages=['life']
+)
